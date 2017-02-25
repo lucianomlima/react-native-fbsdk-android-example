9b646ba18448493c58b7c32831cfea715fb099c6
Luciano Lima <luciano.lima@convergenceworks.com.br>
2017-02-25 18:53:19 -0300

    Add example app files

---
+++ index.android.js
@@ -0,0 +1,114 @@
+/**
+* Sample React Native App
+* https://github.com/facebook/react-native
+* @flow
+*/
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    TouchableNativeFeedback,
+    View
+} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+import { ShareDialog } from 'react-native-fbsdk';
+
+const PICKER_OPTIONS = {
+    mediaType: 'photo',
+    noData: true,
+    quality: 0.75,
+    title: 'Choose photo',
+};
+
+export default class FBSDKTest extends Component {
+    // Share the link using the share dialog.
+    _sharePhotoWithShareDialog = shareContent => {
+        ShareDialog.canShow(shareContent).then(
+            function(canShow) {
+                if (canShow) {
+                    return ShareDialog.show(shareContent);
+                }
+            }
+        ).then(
+            function(result) {
+                console.log(result);
+                if (result.isCancelled) {
+                    alert('Share cancelled');
+                } else {
+                    alert('Share success with postId: '
+                    + result.postId);
+                }
+            },
+            function(error) {
+                alert('Share fail with error: ' + error);
+            }
+        );
+    }
+
+    _showImageGallery = () => {
+        ImagePicker.launchImageLibrary(PICKER_OPTIONS, (response)  => {
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            }
+            else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            }
+            else {
+                // Build up a shareable photo.
+                const sharePhotoContent = {
+                    contentType: 'photo',
+                    photos: [
+                        {
+                            imageUrl: response.uri,
+                            userGenerated: false,
+                            caption: "FBSDK test on Android"
+                        }
+                    ]
+                };
+
+                this._sharePhotoWithShareDialog(sharePhotoContent);
+            }
+        });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <TouchableNativeFeedback onPress={() => this._showImageGallery()}>
+                    <View style={styles.button}>
+                        <Text style={styles.buttonText}>
+                            FBSDK Test
+                        </Text>
+                    </View>
+                </TouchableNativeFeedback>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    button: {
+        backgroundColor: '#ccc',
+        borderRadius: 2,
+        elevation: 2
+    },
+    buttonText: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+AppRegistry.registerComponent('FBSDKTest', () => FBSDKTest);
